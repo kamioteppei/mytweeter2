1024bf0fd0d85e8993fc1c8808c65ff85cc5e4fc
vagrant <vagrant@localhost.localdomain>
2018-08-02 12:58:39 +0900

    modified route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root :to => 'index#index'
+root :to => 'tweets#index'
 
 end
